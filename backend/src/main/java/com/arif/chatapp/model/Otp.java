b02c76faecfb55b1442778ee2c2666fb0d3955fe
ARIF\arifa <arif18052@gmail.com>
2026-03-22 18:06:48 +0530

    Added OTP entity

---
+++ backend/src/main/java/com/arif/chatapp/model/Otp.java
@@ -0,0 +1,38 @@
+package com.arif.chatapp.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "otps")
+public class Otp {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String email;
+
+	@Column(nullable = false)
+	private String otpCode;
+
+	@Column(nullable = false)
+	private Instant expiresAt;
+}
