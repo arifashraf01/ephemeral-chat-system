ecfeed2a2db6d43b9a6042968bbddd2cc41880ff
ARIF\arifa <arif18052@gmail.com>
2026-03-23 14:53:44 +0530

    Added ChatRequest service skeleton

---
+++ backend/src/main/java/com/arif/chatapp/service/ChatRequestService.java
@@ -0,0 +1,19 @@
+package com.arif.chatapp.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChatRequestService {
+
+    public void sendRequest(Long senderId, Long receiverId) {
+        // TODO: implement send request logic
+    }
+
+    public void acceptRequest(Long requestId, Long userId) {
+        // TODO: implement accept request logic
+    }
+
+    public void rejectRequest(Long requestId, Long userId) {
+        // TODO: implement reject request logic
+    }
+}
