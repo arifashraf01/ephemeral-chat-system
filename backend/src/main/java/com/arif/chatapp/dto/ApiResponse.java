8f799225e3d5d55fcd89f6a3878b2723ea7ed0b0
ARIF\arifa <arif18052@gmail.com>
2026-03-23 15:29:11 +0530

    Added standard API response structure

---
+++ backend/src/main/java/com/arif/chatapp/dto/ApiResponse.java
@@ -0,0 +1,17 @@
+package com.arif.chatapp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse<T> {
+
+	private String message;
+	private boolean success;
+	private T data;
+}
