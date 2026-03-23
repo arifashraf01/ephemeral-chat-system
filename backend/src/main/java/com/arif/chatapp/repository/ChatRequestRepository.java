7179f2b497d24956bb8228a2d07a0f11db9f9fea
ARIF\arifa <arif18052@gmail.com>
2026-03-23 14:51:33 +0530

    Added ChatRequest repository

---
+++ backend/src/main/java/com/arif/chatapp/repository/ChatRequestRepository.java
@@ -0,0 +1,15 @@
+package com.arif.chatapp.repository;
+
+import com.arif.chatapp.model.ChatRequest;
+import com.arif.chatapp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ChatRequestRepository extends JpaRepository<ChatRequest, Long> {
+
+	Optional<ChatRequest> findBySenderAndReceiver(User sender, User receiver);
+
+	List<ChatRequest> findByReceiver(User receiver);
+}
