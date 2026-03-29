71c0143fa87ed4adb067360d6bc83b4a0d6b78ac
ARIF\arifa <arif18052@gmail.com>
2026-03-29 17:21:43 +0530

    add protected routes with JWT check

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
+import ProtectedRoute from './ProtectedRoute'
 import Chat from './pages/Chat'
 import Login from './pages/Login'
 import Requests from './pages/Requests'
@@ -10,8 +11,22 @@ function App() {
       <Route path="/" element={<Login />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
-      <Route path="/requests" element={<Requests />} />
-      <Route path="/chat" element={<Chat />} />
+      <Route
+        path="/requests"
+        element={(
+          <ProtectedRoute>
+            <Requests />
+          </ProtectedRoute>
+        )}
+      />
+      <Route
+        path="/chat"
+        element={(
+          <ProtectedRoute>
+            <Chat />
+          </ProtectedRoute>
+        )}
+      />
     </Routes>
   )
 }
