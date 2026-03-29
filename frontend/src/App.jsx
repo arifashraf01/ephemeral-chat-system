1ba7b2af37a6a87e7fc3cd7d1d3928efd1a802f2
ARIF\arifa <arif18052@gmail.com>
2026-03-29 17:13:55 +0530

    connect routes with page components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
+import { Routes, Route } from 'react-router-dom'
+import Chat from './pages/Chat'
+import Login from './pages/Login'
+import Requests from './pages/Requests'
+import Signup from './pages/Signup'
+
 function App() {
   return (
-    <div>
-      <h1>Chat App</h1>
-    </div>
-  );
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/requests" element={<Requests />} />
+      <Route path="/chat" element={<Chat />} />
+    </Routes>
+  )
 }
 
-export default App;
+export default App
