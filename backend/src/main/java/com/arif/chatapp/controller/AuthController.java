6350c67fc3e82c6502ba041313e4d08dd699c341
ARIF\arifa <arif18052@gmail.com>
2026-07-03 21:36:06 +0530

    refactor(backend): use Map import instead of fully qualified name

--- backend/src/main/java/com/arif/chatapp/controller/AuthController.java
+++ backend/src/main/java/com/arif/chatapp/controller/AuthController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
@@ -33,7 +34,7 @@ public class AuthController {
     }
 
     @PostMapping(value = "/login", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public java.util.Map<String, String> login(@Valid @RequestBody LoginRequest request) {
+    public Map<String, String> login(@Valid @RequestBody LoginRequest request) {
         log.info("Login request received: email='{}'", request.getEmail());
         String token = authService.login(request.getEmail(), request.getPassword());
         return java.util.Map.of("token", token);
